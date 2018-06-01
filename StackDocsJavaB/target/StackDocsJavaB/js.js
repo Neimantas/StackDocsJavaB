5638821609f62d582003e8cf1cb47f041a23deee
Laimonas <lmatukas@gmail.com>
2018-06-01 14:46:25 +0300

    neaisku kas dabr bus

--- StackDocsJavaB/target/StackDocsJavaB/js.js
+++ StackDocsJavaB/target/StackDocsJavaB/js.js
@@ -7,13 +7,16 @@ var puslapio_numeris = document.getElementById("puslapio-numeris");
 
 function funkcija_atgal() {
 
-    console.log("mygtukas" + mygtukas_atgal + "veikia ")
-    console.log("kalba" + kalbos_pasirinkimas)
-    console.log("tekstas" + teksto_laukas)
-    console.log(" numeris" + puslapio_numeris)
+    // console.log("mygtukas" + mygtukas_atgal + "veikia ")
+    // console.log("kalba" + kalbos_pasirinkimas)
+    // console.log("tekstas" + teksto_laukas)
+    // console.log(" numeris" + puslapio_numeris)
 
 }
 
 function funkcija_pirmyn() {
-    console.log("mygtukas" + mygtukas_pirmyn + "veikia ")
+    // console.log("mygtukas" + mygtukas_pirmyn + "veikia ")
+
+
 }
+
