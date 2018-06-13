ac63cbfb965cfe790c0e6f98db04a177e308dadb
Motiejus <motiejus.simonis@gmail.com>
2018-06-13 15:58:39 +0300

    M.S.[06.13] Before vas 'ups', now its OK.

--- StackDocsJavaB/src/main/Services/Pagination.java
+++ StackDocsJavaB/src/main/Services/Pagination.java
@@ -19,21 +19,21 @@ public class Pagination {
 
     //gali buti pagerintas kad grazintu DTO, pvz PaginationDTO
     //reikia dar vieno kintamojo kuris butu ieskojimai, pvz pagal kalbas, ir dar vieno search field
-    public List<Topic> listOfThemes(int pageNumber) {
+    public List<Topic> listOfThemes(int pageNumber) { // 3
         TopicsDTO topicsDTO = hs.getAllTopics();
         DocTagsDTO docTagsDTO = hs.getAllDocTags();
         if (topicsDTO.isSuccess() && docTagsDTO.isSuccess()) {
             List<TopicsDAL> listT = topicsDTO.getData();
             List<DocTagsDAL> listD = docTagsDTO.getData();
             List<Topic> topics = new ArrayList<>();
-            int counter = (pageNumber - 1) * listSize;
+            int counter = (pageNumber - 1) * listSize; // 20
             for (int i = 0; i < listT.size(); i++) {
-                if (pageNumber * listSize > counter) {
+                if (pageNumber * listSize > counter) { // 30 > 20
                     Topic dt = new Topic();
-                    dt.setId(listT.get(i).getId());
-                    dt.setTitle(listT.get(i).getTitle());
+                    dt.setId(listT.get(counter).getId());
+                    dt.setTitle(listT.get(counter).getTitle());
                     for (int j = 0; j < listD.size() ; j++) {
-                        if (listT.get(i).getDocTagId() == listD.get(j).getId())
+                        if (listT.get(counter).getDocTagId() == listD.get(j).getId())
                         dt.setDocTagTitle(listD.get(j).getTitle());
                     }
                     topics.add(dt);
--- StackDocsJavaB/src/test/PaginationTest.java
+++ StackDocsJavaB/src/test/PaginationTest.java
@@ -10,7 +10,7 @@ public class PaginationTest {
     @Test
     public void pgTest() {
         Pagination pg = new Pagination();
-        List<Topic> list = pg.listOfThemes(1);
+        List<Topic> list = pg.listOfThemes(3);
         for (int i = 0; i < list.size(); i++) {
             System.out.println("Id: " + list.get(i).getId() + ", title: "
                     + list.get(i).getTitle() + ", doctag title: " + list.get(i).getDocTagTitle());
