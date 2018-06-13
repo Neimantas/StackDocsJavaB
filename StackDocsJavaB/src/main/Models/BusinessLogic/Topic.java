b71c67e4936c3d3d22fc415e64101bfd9af84895
Motiejus <motiejus.simonis@gmail.com>
2018-06-13 15:53:38 +0300

    M.S.[06.13] Redone pagination to return Topic (BL)

--- StackDocsJavaB/src/main/Models/BusinessLogic/Topic.java
+++ StackDocsJavaB/src/main/Models/BusinessLogic/Topic.java
@@ -1,103 +1,27 @@
 package Models.BusinessLogic;
 
 public class Topic {
-	private long _id;
-	private long _docTagId;
-	private String _title;
-
-	private String _introductionHtml;
-    private String _syntaxHtml;
-    private String _parametersHtml;
-    private String _remarksHtml;
-
-    private String _introductionMarkdown;
-    private String _syntaxMarkdown;
-    private String _parametersMarkdown;
-    private String _remarksMarkdown;
-
+	private long Id;
+	private String Title;
+    private String DocTagTitle;
 
     public long getId() {
-        return _id;
+        return Id;
     }
 
     public void setId(long id) {
-        _id = id;
+        Id = id;
     }
 
-    public long getDocTagId() {
-        return _docTagId;
+    public String getTitle() { return Title; }
+
+    public void setTitle(String title) { Title = title; }
+
+    public String getDocTagTitle() {
+        return DocTagTitle;
     }
 
-    public void setDocTagId(long docTagId) {
-        _docTagId = docTagId;
+    public void setDocTagTitle(String docTagTitle) {
+        DocTagTitle = docTagTitle;
     }
-
-    public String getTitle() { return _title; }
-
-    public void setTitle(String title) { _title = title; }
-
-    public String getIntroductionHtml() {
-        return _introductionHtml;
-    }
-
-    public void setIntroductionHtml(String introductionHtml) {
-        _introductionHtml = introductionHtml;
-    }
-
-    public String getSyntaxHtml() {
-        return _syntaxHtml;
-    }
-
-    public void setSyntaxHtml(String syntaxHtml) {
-        _syntaxHtml = syntaxHtml;
-    }
-
-    public String getParametersHtml() {
-        return _parametersHtml;
-    }
-
-    public void setParametersHtml(String parametersHtml) {
-        _parametersHtml = parametersHtml;
-    }
-
-    public String getRemarksHtml() {
-        return _remarksHtml;
-    }
-
-    public void setRemarksHtml(String remarksHtml) {
-        _remarksHtml = remarksHtml;
-    }
-
-    public String getIntroductionMarkdown() {
-        return _introductionMarkdown;
-    }
-
-    public void setIntroductionMarkdown(String introductionMarkdown) {
-        _introductionMarkdown = introductionMarkdown;
-    }
-
-    public String getSyntaxMarkdown() {
-        return _syntaxMarkdown;
-    }
-
-    public void setSyntaxMarkdown(String syntaxMarkdown) {
-        _syntaxMarkdown = syntaxMarkdown;
-    }
-
-    public String getParametersMarkdown() {
-        return _parametersMarkdown;
-    }
-
-    public void setParametersMarkdown(String parametersMarkdown) {
-        _parametersMarkdown = parametersMarkdown;
-    }
-
-    public String getRemarksMarkdown() {
-        return _remarksMarkdown;
-    }
-
-    public void setRemarksMarkdown(String remarksMarkdown) {
-        _remarksMarkdown = remarksMarkdown;
-    }
-
 }
