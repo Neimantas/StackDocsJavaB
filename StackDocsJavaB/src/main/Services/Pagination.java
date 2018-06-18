6eda6d622f0eadb9abf2f8f765af04c9c2b68aae
Motiejus <motiejus.simonis@gmail.com>
2018-06-18 21:43:56 +0300

    M.S.[06.18] Pgination now have search mechanism, but not tested yet

--- StackDocsJavaB/src/main/Services/Pagination.java
+++ StackDocsJavaB/src/main/Services/Pagination.java
@@ -9,32 +9,35 @@ import Models.DAL.TopicsDAL;
 import Models.DTO.DocTagsDTO;
 import Models.DTO.TopicsDTO;
 import Services.Impl.HigherService;
+
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Pagination {
 
     private int listSize = 10;
-    private HigherService hs = new HigherService();
+    private IHigherService hs = new HigherService();
 
     //gali buti pagerintas kad grazintu DTO, pvz PaginationDTO
     //reikia dar vieno kintamojo kuris butu ieskojimai, pvz pagal kalbas, ir dar vieno search field
-    public List<Topic> listOfThemes(int pageNumber) { // 3
+    public List<Topic> listOfThemes(int pageNumber, String docTagid, String searchQuery) {
         TopicsDTO topicsDTO = hs.getAllTopics();
         DocTagsDTO docTagsDTO = hs.getAllDocTags();
         if (topicsDTO.isSuccess() && docTagsDTO.isSuccess()) {
-            List<TopicsDAL> listT = topicsDTO.getData();
             List<DocTagsDAL> listD = docTagsDTO.getData();
+            List<TopicsDAL> listT = searchValues(topicsDTO.getData(), listD, docTagid, searchQuery);
             List<Topic> topics = new ArrayList<>();
-            int counter = (pageNumber - 1) * listSize; // 20
+            int counter = (pageNumber - 1) * listSize;
             for (int i = 0; i < listT.size(); i++) {
-                if (pageNumber * listSize > counter) { // 30 > 20
+                if (pageNumber * listSize > counter) {
                     Topic dt = new Topic();
                     dt.setId(listT.get(counter).getId());
                     dt.setTitle(listT.get(counter).getTitle());
-                    for (int j = 0; j < listD.size() ; j++) {
+                    for (int j = 0; j < listD.size(); j++) {
                         if (listT.get(counter).getDocTagId() == listD.get(j).getId())
-                        dt.setDocTagTitle(listD.get(j).getTitle());
+                            dt.setDocTagTitle(listD.get(j).getTitle());
                     }
                     topics.add(dt);
                     counter++;
@@ -44,5 +47,35 @@ public class Pagination {
         } else {
             return null;
         }
+    }
+
+    private List<TopicsDAL> searchValues(List<TopicsDAL> listT, List<DocTagsDAL> listD, String docTagId, String searchQuery) {
+        List<TopicsDAL> tempT = new ArrayList<>();
+        //If docTags exist, reducing list to chosen language by docTagId
+        if (docTagId != null) {
+            for (int i = 0; i < listT.size(); i++) {
+                if (listT.get(i).getDocTagId() == Long.parseLong(docTagId)) {
+                    tempT.add(listT.get(i));
+                }
+            }
+        } else {
+            tempT = listT;
+        }
+        //If searchQuery exists, searching if list of Topics contains query's
+        List<TopicsDAL> newTemp = new ArrayList<>();
+        if (searchQuery != null) {
+            String[] querys = searchQuery.split(" ");
+            for (int i = 0; i < tempT.size(); i++) {
+                for (int j = 0; j < querys.length; j++) {
+                    if (hs.getDocTagById("" + tempT.get(i).getDocTagId()).getData().get(0).getTitle().equals(querys[j]) || tempT.get(i).getTitle().equals(querys[j])) {
+                        newTemp.add(tempT.get(i));
+                    }
+                }
+            }
+        } else {
+            newTemp = tempT;
+        }
+
+        return newTemp;
     }
 }
