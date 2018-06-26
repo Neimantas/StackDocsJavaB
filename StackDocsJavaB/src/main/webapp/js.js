93b26093467dada8ac82530dfc8ff0957ac8df12
Justas Seskevicius <justinas.seskevicius@gmail.com>
2018-06-26 12:25:54 +0300

    js.js tweaks

--- StackDocsJavaB/src/main/webapp/js.js
+++ StackDocsJavaB/src/main/webapp/js.js
@@ -2,8 +2,7 @@
 function vykstaPaieska() {
     var kalba = document.getElementById("kalba").value;
     var paieskos_laukas = document.getElementById("paieskos-laukas").value;
-    var url = "/servletindex?kalba=" + kalba + "&paieska=" + paieskos_laukas;
-    location.href = url;
+    location.href = "/servletindex?kalba=" + kalba + "&paieska=" + paieskos_laukas;
 }
 
 
@@ -16,12 +15,14 @@ function changePage(id){
     var kalba = GetParam("kalba");
     var paieskos_laukas = getParam("paieska");
     var puslapis = parseInt(getParam("puslapis"));
-    if (puslapis === -1) {
-        puslapis = 1;
-    }
-    if (id == "next") {
+    if (puslapis >= 1) {
         puslapis++;
-    } else if (id == "previous") {
+    } else {
+        return;
+    }
+    if (id === "next") {
+        puslapis++;
+    } else if (id === "previous") {
         if (puslapis > 1) {
             puslapis--;
         } else {
@@ -31,29 +32,29 @@ function changePage(id){
     location.href = "/servletindex?kalba=" + kalba + "&paieska=" + paieskos_laukas + "&puslapis" + puslapis;
 }
 
-function funkcija_pirmyn() {
-    // Need to check whether there actually is a list to move through
-    var kalba = GetParam("kalba");
-    var paieskos_laukas = getParam("paieska");
-    var puslapis = parseInt(getParam("puslapis"));
-    if (puslapis === -1) {
-        puslapis = 1;
-    }
-    puslapis += 1;
-    location.href = "/servletindex?kalba=" + kalba + "&paieska=" + paieskos_laukas + "&puslapis=" + puslapis;
-
-}
-
-function funkcija_atgal() {
-    var kalba = document.getElementById("kalba").value;
-    var paieskos_laukas = document.getElementById("paieskos-laukas").value;
-    var puslapis = parseInt(getParam("puslapis"));
-    var url;
-    if (puslapis > 1) {
-        puslapis -= 1;
-        location.href = "/servletindex?kalba=" + kalba + "&paieska=" + paieskos_laukas + "&puslapis=" + puslapis;
-    }
-}
+// function funkcija_pirmyn() {
+//     // Need to check whether there actually is a list to move through
+//     var kalba = GetParam("kalba");
+//     var paieskos_laukas = getParam("paieska");
+//     var puslapis = parseInt(getParam("puslapis"));
+//     if (puslapis === -1) {
+//         puslapis = 1;
+//     }
+//     puslapis += 1;
+//     location.href = "/servletindex?kalba=" + kalba + "&paieska=" + paieskos_laukas + "&puslapis=" + puslapis;
+//
+// }
+//
+// function funkcija_atgal() {
+//     var kalba = document.getElementById("kalba").value;
+//     var paieskos_laukas = document.getElementById("paieskos-laukas").value;
+//     var puslapis = parseInt(getParam("puslapis"));
+//     var url;
+//     if (puslapis > 1) {
+//         puslapis -= 1;
+//         location.href = "/servletindex?kalba=" + kalba + "&paieska=" + paieskos_laukas + "&puslapis=" + puslapis;
+//     }
+// }
 
 function getParam(parameter) {
     var urlParams = new URLSearchParams(window.location.search);
