aa2dbc3388543de5aa511be025aecef4d1c59ba5
lm <lmatukas@gmail.com>
2018-07-05 10:47:43 +0300

    dropdown edited

--- StackDocsJavaB/src/main/Services/DropDown.java
+++ StackDocsJavaB/src/main/Services/DropDown.java
@@ -5,12 +5,13 @@ import Models.DAL.DocTagsDAL;
 import Models.DTO.DocTagsDTO;
 import Services.Impl.Cache;
 import Services.Impl.HigherService;
+import org.modelmapper.ModelMapper;
 
+import javax.print.Doc;
 import java.util.ArrayList;
 import java.util.List;
 
 public class DropDown {
-
 
     private String getDropDown = "dropdownGet";
     private String[] dropDownsNeeded = {"3", "4", "5", "8"};
@@ -20,51 +21,25 @@ public class DropDown {
     }
 
     public List<DocTag> getList() {
-
         Cache cache = Cache.getInstance();
-
-        List<DocTag> drop = new ArrayList<>();
-
-        Object some = cache.get(getDropDown);
-
+        List<DocTag> docTagsList = new ArrayList<>();
+        Object obj = cache.get(getDropDown);
         if (cache.get(getDropDown) != null) {
-
-            drop = (List<DocTag>) some;
-
-            return drop;
+            docTagsList = (List<DocTag>) obj;
+            return docTagsList;
 
         } else {
-
             IHigherService higher = new HigherService();
-
-            DocTagsDTO dtoList = higher.getDocTagById(dropDownsNeeded);
-
-            List<DocTagsDAL> dropDown = dtoList.getList();
-
-
-            for (int i = 0; i < dropDown.size(); i++) {
-
-                DocTag docTag = new DocTag();
-
-                DocTagsDAL mydal = dropDown.get(i);
-
-                docTag.setId(mydal.getId());
-
-                docTag.setTag(mydal.getTag());
-
-                drop.add(docTag);
-
+            DocTagsDTO docTagsDTO = higher.getDocTagById(dropDownsNeeded);
+            if (docTagsDTO.isSuccess()) {
+                List<DocTagsDAL> docTagsDALsList = docTagsDTO.getList();
+                for (int i = 0; i < docTagsDALsList.size(); i++) {
+                    ModelMapper modelMapper = new ModelMapper();
+                    docTagsList.add(modelMapper.map(docTagsDALsList.get(i) ,DocTag.class));
+                }
             }
-
-            cache.put(getDropDown, drop);
-
-            return drop;
-
+            cache.put(getDropDown, docTagsList);
+            return docTagsList;
         }
-
     }
-
 }
-
-
-
