9a08822de77cc4943c96faf53770a909bef5b384
Motiejus <motiejus.simonis@gmail.com>
2018-07-16 14:10:28 +0300

    M.S. Constant for cache created, and Cache.class corected, no more stupid coments

--- StackDocsJavaB/src/main/Models/CONS/Settings.java
+++ StackDocsJavaB/src/main/Models/CONS/Settings.java
@@ -2,7 +2,9 @@ package Models.CONS;
 
 public enum Settings {
     //Its the amount of size sent to frontEnd, and its equals amount of lines of topics will be displayed after any search
-    LIST_SIZE(10);
+    LIST_SIZE(10),
+    //Its the amount of time items will be saved in Cache.class, now its set to 120 seconds
+    LIVE_TIME(120000);
 
     Settings(int Data) {
         data = Data;
