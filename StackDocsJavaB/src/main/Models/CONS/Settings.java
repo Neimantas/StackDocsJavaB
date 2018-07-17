8c86c13201b774610e5722f9f901c20fd9fab019
Laimonas <lmatukas@gmail.com>
2018-07-17 13:33:37 +0300

    Settings class

--- StackDocsJavaB/src/main/Models/CONS/Settings.java
+++ StackDocsJavaB/src/main/Models/CONS/Settings.java
@@ -1,22 +1,12 @@
 package Models.CONS;
 
-public enum Settings {
+public final class Settings {
     //Its the amount of size sent to frontEnd, and its equals amount of lines of topics will be displayed after any search
-    LIST_SIZE(10),
+    public static int LIST_SIZE = 10;
     //Its the amount of time items will be saved in Cache.class, now its set to 120 seconds
-    LIVE_TIME(120000),
-    //  DROPDOWN to cash KEY
-    DROPDOWN_CASH("dropdownGet"),
+    public static long LIVE_TIME = 120000;
+    //  DROPDOWN to cache KEY
+    public static String DROPDOWN_CACHE = "dropdownGet";
     // DROPDOWN languages list
-    DROPDOWN_LANGUAGES(new String[]{"3", "4", "5", "8"});
-
-    Settings(Object Settings) {
-        settings = Settings;
-    }
-
-    private final Object settings;
-
-    public Object get() {
-        return settings;
-    }
+    public static String[] DROPDOWN_LANGUAGES = {"3", "4", "5", "8"};
 }
